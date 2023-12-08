0c4a7dddbb7ce7971df65afc05a047a8f2368eb3
FeirieO <feirie001@gmail.com>
2023-12-08 10:38:09 +0000

    Prints the string rep of an instance based on the class name

--- console.py
+++ console.py
@@ -24,10 +24,28 @@ class HBNBCommand(cmd.Cmd):
 	splitline = line.split()
 	if not splitline:
 		print("** user name missing **")
-	elif splitline[0] not in new_user:
+	elif splitline[0] not in new_classes:
 		print("** class doesn't exist **")
 	else:
-		new_instance = new_user[splitline[0]]()
+		new_instance = new_classes[splitline[0]]()
 		print(new_instance.id)
 		new_instance.save()
 
+    def show(self, line):
+	"""Show command to show an instance based on User name and id"""
+	if not line:
+		print("** class name missing **")
+	elif line.split()[0] not in new_classes:
+		print("** class doesn't exist **")
+	elif len(line.split()) < 2:
+		print("** instance id missing **")
+	else:
+		class_name = line.split()[0]
+		instance_id = line.split()[1]
+		new_instance =  "{}.{}".format(class_name, instance_id)
+		objs = models.storage.all()
+
+	if new_instance not in objs:
+		print("** no instance found **")
+	else:
+		print(objs[new_instance]) 
