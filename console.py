3d636e1ebd0c1741ac83c081aa94370dac8ba7ad
FeirieO <feirie001@gmail.com>
2023-12-08 11:12:00 +0000

    Updates an instance based on the class name and id by adding or updating

--- console.py
+++ console.py
@@ -35,10 +35,13 @@ class HBNBCommand(cmd.Cmd):
 	"""Show command to show an instance based on User name and id"""
 	if not line:
 		print("** class name missing **")
+
 	elif line.split()[0] not in new_classes:
 		print("** class doesn't exist **")
+
 	elif len(line.split()) < 2:
 		print("** instance id missing **")
+
 	else:
 		class_name = line.split()[0]
 		instance_id = line.split()[1]
@@ -48,4 +51,84 @@ class HBNBCommand(cmd.Cmd):
 	if new_instance not in objs:
 		print("** no instance found **")
 	else:
-		print(objs[new_instance]) 
+		print(objs[new_instance])
+
+    def destroy(self, line):
+	"""Delete command to delete an instance based on class name and id"""
+    	splitline = line.split()
+
+    	if not splitline:
+        	print("** class name missing **")
+        	return False
+
+    	elif splitline[0] not in new_classes:
+        	print("** class doesn't exist **")
+
+    	elif len(splitline) < 2:
+        	print("** instance id missing **")
+
+    	else:
+        	class_name = splitline[0]
+       		instance_id = splitline[1]
+        	new_instance = "{}.{}".format(class_name, instance_id)
+        	all_objects = models.storage.all()
+
+        if new_instance not in all_objects:
+            	print("** no instance found **")
+        else:
+            	del all_objects[new_instance]
+            	models.storage.save()
+
+    def all(self, line):
+    	"""All command to print all instances based or not on class name"""
+    	str_list = []
+    	all_objects = models.storage.all()
+
+    	if not line:
+        	for new_instance in all_objects.values():
+            	str_list.append(str(new_instance))
+    	else:
+        	splitline = line.split()
+        	if splitline[0] in new_classes:
+            		for key, value in all_objects.items():
+                	if value.__class__.__name__ == splitline[0]:
+                    		str_list.append(str(value))
+        	else:
+            		print("** class doesn't exist **")
+            		return False
+    	print(str_list)
+
+    def update(self, line):
+        """Update command to update an instance base on class name and id"""
+        splitline = split(line)
+
+        if not splitline:
+        	print("** class name missing **")
+
+        elif splitline[0] not in new_classes:
+            	print("** class doesn't exist **")
+
+        elif len(splitline) < 2:
+            	print("** instance id missing **")
+
+        elif len(splitline) < 3:
+            	print("** attribute name missing **")
+
+        elif len(splitline) < 4:
+            	print("** value missing **")
+
+        else:
+            	new_instance = splitline[0] + '.' + splitline[1]
+            	if new_instance not in models.storage.all():
+                	print("** no instance found **")
+            	else:
+                	setattr(models.storage.all()[new_instance],
+                        	splitline[2], splitline[3])
+                	models.storage.save()
+
+
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
